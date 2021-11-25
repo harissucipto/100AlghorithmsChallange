6399f57f6f6b4ab8b78dad3155402c7b245354ad
haris sucipto <harisssucipto@gmail.com>
2021-11-25 20:41:25 +0700

    solve add

--- add/add.spec.ts
+++ add/add.spec.ts
@@ -1,44 +1,44 @@
-import { add, add2 } from './add';
+import { add, add2 } from "./add";
 
-xdescribe(add.name, () => {
-    it('test 1', () => {
-        // act
-        const result = add(1, 2);
+describe(add.name, () => {
+  it("test 1", () => {
+    // act
+    const result = add(1, 2);
 
-        // assert
-        expect(result).toBe(3);
-    });
+    // assert
+    expect(result).toBe(3);
+  });
 
-    it('test 2', () => {
-        // act
-        const result = add(3, 2);
+  it("test 2", () => {
+    // act
+    const result = add(3, 2);
 
-        // assert
-        expect(result).toBe(5);
-    });
+    // assert
+    expect(result).toBe(5);
+  });
 });
 
-xdescribe(add2.name, () => {
-    // console.log(add2(2,3));
-    it('test 1', () => {
-        // arrange
-        const data = [1, 2, 3, 4, 5];
+describe(add2.name, () => {
+  // console.log(add2(2,3));
+  it("test 1", () => {
+    // arrange
+    const data = [1, 2, 3, 4, 5];
 
-        // act
-        const result = add2(...data);
+    // act
+    const result = add2(...data);
 
-        // assert
-        expect(result).toBe(15);
-    });
+    // assert
+    expect(result).toBe(15);
+  });
 
-    it('test 2', () => {
-        // arrange
-        const data = [2, 3];
+  it("test 2", () => {
+    // arrange
+    const data = [2, 3];
 
-        // act
-        const result = add2(...data);
+    // act
+    const result = add2(...data);
 
-        // assert
-        expect(result).toBe(5);
-    });
+    // assert
+    expect(result).toBe(5);
+  });
 });
--- add/add.ts
+++ add/add.ts
@@ -1,9 +1,11 @@
 export function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
-
+  return param1.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
 }
 // console.log(add(1, 2));
 // console.log(add(3, 2));
