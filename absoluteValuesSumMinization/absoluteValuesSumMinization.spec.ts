94ef99966cb3eb0fa22d19653f783231f53bd166
haris sucipto <harisssucipto@gmail.com>
2021-11-25 20:52:17 +0700

    solve: absoluteValueSumMinization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.spec.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.spec.ts
@@ -1,47 +1,47 @@
-import { absoluteValuesSumMinimization } from './absoluteValuesSumMinization';
+import { absoluteValuesSumMinimization } from "./absoluteValuesSumMinization";
 
-xdescribe(absoluteValuesSumMinimization.name, () => {
-    it('Test 1', () => {
-        // arrange
-        const data = [2, 4, 7];
+describe(absoluteValuesSumMinimization.name, () => {
+  it("Test 1", () => {
+    // arrange
+    const data = [2, 4, 7];
 
-        // act
-        const response = absoluteValuesSumMinimization(data);
+    // act
+    const response = absoluteValuesSumMinimization(data);
 
-        // assert
-        expect(response).toBe(4);
-    });
+    // assert
+    expect(response).toBe(4);
+  });
 
-    it('Test 2', () => {
-        // arrange
-        const data = [2, 4, 7, 6];
+  it("Test 2", () => {
+    // arrange
+    const data = [2, 4, 7, 6];
 
-        // act
-        const response = absoluteValuesSumMinimization(data);
+    // act
+    const response = absoluteValuesSumMinimization(data);
 
-        // assert
-        expect(response).toBe(4);
-    });
+    // assert
+    expect(response).toBe(4);
+  });
 
-    it('Test 3', () => {
-        // arrange
-        const data = [2, 4, 7, 6, 6];
+  it("Test 3", () => {
+    // arrange
+    const data = [2, 4, 7, 6, 6];
 
-        // act
-        const response = absoluteValuesSumMinimization(data);
+    // act
+    const response = absoluteValuesSumMinimization(data);
 
-        // assert
-        expect(response).toBe(7);
-    });
+    // assert
+    expect(response).toBe(7);
+  });
 
-    it('Test 4', () => {
-        // arrange
-        const data = [2, 4, 7, 6, 6, 8];
+  it("Test 4", () => {
+    // arrange
+    const data = [2, 4, 7, 6, 6, 8];
 
-        // act
-        const response = absoluteValuesSumMinimization(data);
+    // act
+    const response = absoluteValuesSumMinimization(data);
 
-        // assert
-        expect(response).toBe(7);
-    });
+    // assert
+    expect(response).toBe(7);
+  });
 });
--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,12 @@
+// find median value from x
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+  const isEven = a.length % 2 === 0;
+  const index = isEven ? a.length / 2 - 1 : Math.floor(a.length / 2);
+
+  return a[index];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
